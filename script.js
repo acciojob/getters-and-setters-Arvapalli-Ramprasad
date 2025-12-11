1eeb4f1d2dc3f736ae8835e862e12368088cc92a
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2025-12-11 10:59:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-//complete this code
 class Person {
-	constructor(name, age){
-		this.name = name;
-		this.age = age;
-	}
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-	getName(){
-		return name;
-	}
+    getName() {
+        return this.name;
+    }
 
-	setAge(age){
-		this.age = age;
-	}
+    setAge(age) {
+        this.age = age;
+    }
 }
 
-
 class Student extends Person {
-	constructor(name, age){
-		super(name, age);
-	}
-	
-	study(){
-		console.log('${this.name} is studying');
-	}
+    constructor(name, age) {
+        super(name, age); // Call parent constructor
+    }
+
+    study() {
+        console.log(`${this.name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-	constructor(name, age){
-		super(name, age);
-	}
-	tech(){
-		console.log('{this.name} is teaching')
-	}
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
 }
 
 // Do not change the code below this line
